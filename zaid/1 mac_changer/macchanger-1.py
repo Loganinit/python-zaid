1ef26bde3b34225118d26723c53c26d1dca3a244
root <root@localhost.localdomain>
2018-11-17 20:04:02 +0530

    Updated Replace Downloads

--- zaid/1 mac_changer/macchanger-1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python2
-
-import subprocess
-
-subprocess.call("ifconfig enp2s0 down",shell=True)
-subprocess.call("ifconfig enp2s0 hw ether 00:11:22:33:44:55",shell=True)
-subprocess.call("ifconfig enp2s0 up",shell=True)
